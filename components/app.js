d58f09061b1a35fa5856fda07ea959f9ab9ecb79
Seong Lee <seongkevinlee@gmail.com>
2020-05-06 20:09:52 -0700

    Completed View Average Grade

--- components/app.js
+++ components/app.js
@@ -1,16 +1,22 @@
 class App {
-
   constructor(gradeTable, pageHeader) {
-    this.handleGetGradesError = this.handleGetGradesError.bind(this);
-    this.handleGetGradesSuccess = this.handleGetGradesSuccess.bind(this);
     this.gradeTable = gradeTable;
     this.pageHeader = pageHeader;
+    this.handleGetGradesError = this.handleGetGradesError.bind(this);
+    this.handleGetGradesSuccess = this.handleGetGradesSuccess.bind(this);
   }
   handleGetGradesError(error) {
     console.error(error);
   }
   handleGetGradesSuccess(grades) {
     this.gradeTable.updateGrades(grades);
+    var average = 0;
+    var total = 0;
+    for (var index = 0; index < grades.length; index++){
+      total += grades[index].grade;
+      }
+      average = Math.floor(total / grades.length);
+    this.pageHeader.updateAverage(average);
   }
   getGrades() {
     $.ajax({
--- components/page-header.js
+++ components/page-header.js
@@ -3,6 +3,7 @@ class PageHeader {
     this.headerElement = headerElement;
   }
   updateAverage(newAverage) {
-    console.log(newAverage);
+    var badgeEl = this.headerElement.querySelector('.badge');
+    badgeEl.textContent = newAverage;
   }
 }
