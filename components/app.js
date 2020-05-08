97d1d477d9fd9f80d83c0b9833d4b0874ed41ca1
Seong Lee <seongkevinlee@gmail.com>
2020-05-07 19:18:40 -0700

    Completed Delete A Grade Feature

--- components/app.js
+++ components/app.js
@@ -6,22 +6,16 @@ class App {
     this.handleGetGradesSuccess = this.handleGetGradesSuccess.bind(this);
     this.gradeForm = gradeForm;
     this.createGrade = this.createGrade.bind(this);
+    this.deleteGrade = this.deleteGrade.bind(this);
     this.handleCreateGradeError = this.handleCreateGradeError .bind(this);
     this.handleCreateGradeSuccess = this.handleCreateGradeSuccess.bind(this);
     this.handleDeleteGradeError = this.handleDeleteGradeError.bind(this);
     this.handleDeleteGradeSuccess = this.handleDeleteGradeSuccess.bind(this);
   }
-  handleGetGradesError(error) {
-    console.error(error);
-  }
-  handleGetGradesSuccess(grades) {
-    this.gradeTable.updateGrades(grades);
-    var total = 0;
-    for (var index = 0 ; index < grades.length ; index++){
-      total += grades[index].grade;
-      }
-    var average = Math.floor(total / grades.length);
-    this.pageHeader.updateAverage(average);
+  start(){
+    this.getGrades();
+    this.gradeForm.onSubmit(this.createGrade);
+    this.gradeTable.onDeleteClick(this.deleteGrade);
   }
   getGrades() {
     $.ajax({
@@ -32,9 +26,17 @@ class App {
       error: this.handleGetGradesError
       })
   }
-  start(){
-    this.getGrades();
-    this.gradeForm.onSubmit(this.createGrade);
+  handleGetGradesSuccess(grades) {
+    this.gradeTable.updateGrades(grades);
+    var total = 0;
+    for (var index = 0 ; index < grades.length ; index++){
+      total += grades[index].grade;
+      }
+    var average = Math.floor(total / grades.length);
+    this.pageHeader.updateAverage(average);
+  }
+  handleGetGradesError(error) {
+    console.error(error);
   }
 
   createGrade(name, course, grade) {
@@ -63,6 +65,14 @@ class App {
 
   deleteGrade(id) {
     console.log(id);
+    $.ajax({
+      method: "DELETE",
+      url: "https://sgt.lfzprototypes.com/api/grades/" + id,
+      data: "none",
+      headers: { "X-Access-Token": "5oM35U7P" },
+      success: this.handleDeleteGradeSuccess,
+      error: this.handleDeleteGradeError
+    })
   }
 
   handleDeleteGradeError(error) {
@@ -70,6 +80,7 @@ class App {
   }
 
   handleDeleteGradeSuccess() {
+    console.log(this.getGrades);
     this.getGrades();
   }
 }
--- main.js
+++ main.js
@@ -5,7 +5,9 @@ var headerEl = document.querySelector('header');
 var header = new PageHeader(headerEl);
 
 var tableEl = document.querySelector("#table-element");
-var gradeTable = new GradeTable(tableEl);
+var noGradesEl = document.querySelector("#no-grades");
+var gradeTable = new GradeTable(tableEl, noGradesEl);
+
 
 var app = new App(gradeTable, header, form);
 app.start();
